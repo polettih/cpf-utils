125b19cbd8fed631013be0ec6d69bfecd8fbcb15
polettih <henrique.poletti@gmail.com>
2025-04-23 16:39:54 -0300

    becoming a paclet

---
+++ cpf_utils/__init__.py
@@ -0,0 +1,2 @@
+from .core import gerar_cpf, validar_cpf
+from cpf_utils import gerar_cpf, validar_cpf
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cpf_utils',
+    version='0.1',
+    packages=find_packages(),
+    description='Funções para a geração e validação de CPF',
+    author='Henrique Poletti',
+    author_email='henrique.poletti@gmail.com',
+    url='https://github.com/polettih/cpf-utils',
+    python_requres='=3.6',
+)
